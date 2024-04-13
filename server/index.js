e9a46cc355de15bdf37060db60421f35fcde5266
devgirlpro <agalidari@gmail.com>
2024-04-13 17:05:55 +0200

    instal cors and use it

--- server/index.js
+++ server/index.js
@@ -2,12 +2,24 @@ import express from 'express';
 import mongoose from 'mongoose';
 import { PORT, mongodbUrl } from './config.js';
 import bookRoute from './routes/book.route.js';
+import cors from 'cors';
 
 
 
 const app = express();
 
 app.use(express.json());
+
+//middleware for handling CORS POLICA
+//option 1: Allow Origins with default of cors(*)
+app.use(cors());
+
+//option 2: Allow Custom Origins
+// app.use(cors({
+//     origin: 'HHTP://localhost:3003',
+//     methods: ['GET', 'POSt', 'PUT', 'DELETE'],
+//     alloweHeaders: ['Content-type']
+// }))
 
 app.get('/', (request, response) => {
     console.log(request);
