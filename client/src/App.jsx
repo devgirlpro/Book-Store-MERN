a75b1225d0b3c2cccf874b0baad95516b8e07fb5
devgirlpro <agalidari@gmail.com>
2024-04-13 17:58:19 +0200

    strat react project as frontend

---
+++ client/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import CreateBook from './pages/CreateBook';
+import DeleteBook from './pages/DeleteBook';
+import EditeBook from './pages/EditeBook';
+import ShowBook from './pages/ShowBook';
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/books/create" element={<CreateBook />} />
+      <Route path="/books/details/:id" element={<ShowBook />} />
+      <Route path="/dooks/delete/:id" element={<DeleteBook />} />
+      
+      <Route path="/books/edite/:id" element={<EditeBook />} />
+    </Routes>
+  )
+}
+
+export default App
