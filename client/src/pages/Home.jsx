2c260c625fa85ceb596b539b57f696dbe71e51a7
devgirlpro <agalidari@gmail.com>
2024-04-15 15:10:46 +0200

    Show Books List as Card - conditional rendering

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,14 +2,16 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Spinner from '../components/Spinner';
 import { Link } from 'react-router-dom';
-import { AiOutlineEdit } from 'react-icons/ai';
-import { BsInfoCircle } from 'react-icons/bs';
-import { MdOutlineAddBox, MdOutlineDelete } from 'react-icons/md';
+
+import { MdOutlineAddBox } from 'react-icons/md';
+import BookTable from '../components/home/BookTable';
+import BookCard from '../components/home/BookCard';
 
 const Home = () => {
   const [books, setBooks] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+  const [showType, setShowType] = useState('table');
 
   useEffect(() => {
     setLoading(true);
@@ -21,7 +23,7 @@ const Home = () => {
         setLoading(false);
       } catch (err) {
         setError(err);
-        console.log(error)
+        console.log(error);
       } finally {
         setLoading(false);
       }
@@ -32,63 +34,34 @@ const Home = () => {
 
   return (
     <div className="p-4">
+      <div className="flex justify-center items-center gap-x-4">
+        <button
+          className="bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-lg"
+          onClick={() => setShowType('table')}
+        >
+          Table
+        </button>
+        <button
+          className="bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-lg"
+          onClick={() => setShowType('card')}
+        >
+          Card
+        </button>
+      </div>
+
       <div className="flex justify-between items-center">
         <h1 className="text-3xl my-8">BookS List</h1>
-        <Link to='/books/create'>
+        <Link to="/books/create">
           <MdOutlineAddBox className="text-sky-600 text-4xl" />
         </Link>
       </div>
+
       {loading ? (
         <Spinner />
+      ) : showType === 'table' ? (
+        <BookTable books={books} />
       ) : (
-        <table className="w-full border-separate border-spacing-2">
-          <thead>
-            <tr>
-              <th className="border border-slate-600 rounded-md">No</th>
-              <th className="border border-slate-600 rounded-md">Title</th>
-              <th className="border border-slate-600 rounded-md max-md:hidden">
-                Author
-              </th>
-              <th className="border border-slate-600 rounded-md max-md:hidden">
-                Published Year
-              </th>
-              <th className="border border-slate-600 rounded-md">Operation</th>
-            </tr>
-          </thead>
-          <tbody>
-            {books.map((book, index) => {
-              return (
-                <tr key={book._id} className="h-8">
-                  <td className="border border-slate-700 rounded-md text-center">
-                    {index + 1}
-                  </td>
-                  <td className="border border-slate-700 rounded-md text-center">
-                    {book.title}
-                  </td>
-                  <td className="border border-slate-700 rounded-md text-center max-md:hidden">
-                    {book.author}
-                  </td>
-                  <td className="border border-slate-700 rounded-md text-center max-md:hidden">
-                    {book.publishYear}
-                  </td>
-                  <td className="border border-slate-700 rounded-md text-center">
-                    <div className="flex justify-center gap-x-4">
-                      <Link to={`/books/details/${book._id}`}>
-                        <BsInfoCircle className="text-2xl text-green-800" />
-                      </Link>
-                      <Link to={`/books/edit/${book._id}`}>
-                        <AiOutlineEdit className="text-2xl text-yellow-600" />
-                      </Link>
-                      <Link to={`/books/delete/${book._id}`}>
-                        <MdOutlineDelete className="text-2xl text-red-600" />
-                      </Link>
-                    </div>
-                  </td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
+        (<BookCard books={books} />)
       )}
     </div>
   );
